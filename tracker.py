23a15d5c9bbad7c817fe93863606d773608989a0
RoCry <crysheen@gmail.com>
2022-05-16 20:11:28 +0800

    in progress

--- tracker.py
+++ tracker.py
@@ -1,10 +1,14 @@
+import os
 from typing import Union
+import pandas as pd
 
 from util import logger
 from arweave import ArweaveFetcher
 
 
 class Tracker(object):
+    transactions_path = "transactions.csv"
+
     def __init__(self, tags: list[dict[str, Union[str, list[str]]]], transformer):
         self.fetcher = ArweaveFetcher(tags=tags, tags_transformer=transformer)
 
@@ -14,3 +18,12 @@ class Tracker(object):
         )
         self.fetcher.last_cursor = cursor
         logger.debug(f"Fetched {len(txs)} transactions")
+        self.save_transactions(txs)
+
+    def save_transactions(self, txs: list[dict]):
+        pd.DataFrame(txs).to_csv(
+            self.transactions_path,
+            mode="a",
+            index=False,
+            header=not os.path.exists(self.transactions_path),
+        )
