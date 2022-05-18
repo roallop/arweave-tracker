95dd3bfd34e8a7502981fd9fded6bfc7e9624a9f
RoCry <crysheen@gmail.com>
2022-05-18 18:33:18 +0800

    fix missing originalDigest

--- arweave.py
+++ arweave.py
@@ -131,7 +131,9 @@ query($cursor: String, $min_block: Int, $tags: [TagFilter!]!, $limit: Int!) {
                 "title": content["title"],
                 "body": content["body"],
                 "timestamp": int(content["timestamp"]),
-                "digest": post["originalDigest"],  # prefer use originalDigest
+                "digest": post.get(
+                    "originalDigest", post["digest"]
+                ),  # prefer use originalDigest
                 "contributor": post["authorship"]["contributor"],
             }
             if nft := post.get("nft"):
